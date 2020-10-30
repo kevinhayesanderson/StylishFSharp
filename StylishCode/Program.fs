8c4a06c03921f672048bc9f6c0f0cf3680d07472
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2020-10-30 20:17:59 +0530

    moving incoming nullable values into the option

--- StylishCode/Program.fs
+++ StylishCode/Program.fs
@@ -25,15 +25,16 @@ let herOrder = {
     Billing = "9 Gravity Road\nErewhon\n80665"
     Delivery = Download } 
 
-let addressForPackage (details : BillingDetails) =
-    let address = details.Delivery |> Option.defaultValue details.Billing
-    sprintf "%s\n%s" details.Name address
+let tryDeliveryLabel (billingDetails : BillingDetails) =
+    match billingDetails.Delivery with
+        | AsBilling -> billingDetails.Billing |> Some
+        | Physical address -> address |> Some
+        | Download -> None
+        |> Option.map (fun address -> sprintf "%s\n%s" billingDetails.Name address)
 
-let printDeliveryAddress (details : BillingDetails) =
-    // don't explicitely match with some or none
-    details.Delivery
-        |> Option.map (fun address -> address.ToUpper())
-        |> Option.iter (fun address -> printfn "%s\n%s" details.Name address)
+let deliveryLabels (billingDetails : BillingDetails seq) =
+            billingDetails 
+                |> Seq.choose tryDeliveryLabel
 
 let tryLastLine (address : string) =
     let parts = address.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries)
@@ -48,19 +49,9 @@ let tryPostalCode (codeString : string) =
 
 let postalCodeHub (code : int) = if code = 62291 then "Hub 1" else "Hub 2"
 
-let tryDeliveryLabel (billingDetails : BillingDetails) =
-    match billingDetails.Delivery with
-        | AsBilling -> billingDetails.Billing |> Some
-        | Physical address -> address |> Some
-        | Download -> None
-        |> Option.map (fun address -> sprintf "%s\n%s" billingDetails.Name address)
-
-let deliveryLabels (billingDetails : BillingDetails seq) =
-    billingDetails 
-        |> Seq.choose tryDeliveryLabel
-
-let tryHub (details : BillingDetails) =
-    details.Delivery
+let tryHub (billingDetails : BillingDetails) =
+    billingDetails
+        |> tryDeliveryLabel
         |> Option.bind tryLastLine
         |> Option.bind tryPostalCode
         |> Option.map postalCodeHub
@@ -71,19 +62,13 @@ let main _ =
     // let milesYards = 1.1 |> MilesYards.fromMilesPointYards |> MilesYards.toDecimalMiles 
     // printfn "%A" milesYards
 
-    printfn "%s" (addressForPackage myOrder)
-
-    printfn "%s" (addressForPackage hisOrder)
-
-    myOrder |> printDeliveryAddress
-
-    hisOrder |> printDeliveryAddress
-
     myOrder |> tryHub |> ignore
 
     hisOrder |> tryHub |> ignore
 
-    [ myOrder; hisOrder; herOrder ] |> deliveryLabels |> ignore
+    let orders = [| myOrder; hisOrder; herOrder |]
+
+    orders |> deliveryLabels |> ignore
 
     let _key = System.Console.ReadKey()
     0
