db468a4443416b410328bd2ea32caba03a1b6e99
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2020-10-31 12:36:12 +0530

    collection init

---
+++ StylishCode/Houses.fs
@@ -0,0 +1,30 @@
+﻿module Houses
+
+type House = { Address : string; Price : decimal }
+
+type PriceBand = | Cheap | Medium | Expensive
+
+let random = System.Random(Seed = 1)
+
+let getHouses count =
+    Array.init count (fun i ->
+            {
+                Address = sprintf "%i Stochastic Street" (i+1)
+                Price = random.Next(50_000, 500_000) |> decimal
+            }
+    )
+
+let trySchoolDistance (house : House) =
+    let dist = random.Next(10) |> double
+    if dist < 8. then
+        Some dist
+    else
+        None
+
+let priceBand (price : decimal) =
+    if price < 100_000m then
+        Cheap
+    else if price < 200_000m then
+        Medium
+    else
+        Expensive
---
+++ StylishCode/Order.fs
@@ -0,0 +1,85 @@
+﻿module Order
+
+open System
+
+type Delivery =
+    | AsBilling
+    | Physical of string
+    | Download
+    | ClickAndCollect of int
+
+type BillingDetails = {
+    Name : string
+    Billing : string
+    Delivery : Delivery }
+
+let myOrder = { 
+    Name = "Kit Eason" 
+    Billing = "112 Fibonacci Street\nErehwon\n35813" 
+    Delivery = AsBilling }
+
+let hisOrder = {
+    Name = "John Doe" 
+    Billing = "314 Pi Avenue\nErewhon\n15926" 
+    Delivery = Physical "16 Planck Parkway\nErewhon\n62291" }
+
+let herOrder = {
+    Name = "Jane Smith"
+    Billing = "9 Gravity Road\nErewhon\n80665"
+    Delivery = Download } 
+
+let yourOrder = {
+    Name = "Alison Chan"
+    Billing = "885 Electric Avenue\nErewhon\n41878"
+    Delivery = ClickAndCollect 1 }
+
+let theirOrder = {
+    Name = "Pana Okpik"
+    Billing = "299 Relativity Drive\nErewhon\79245"
+    Delivery = ClickAndCollect 2 }
+
+let collectionsFor (storeId : int) (billingDetails : BillingDetails seq) =
+    billingDetails
+        |> Seq.choose (fun d ->
+            match d.Delivery with
+                | ClickAndCollect s when s = storeId -> Some d
+                | _ -> None)
+
+let tryDeliveryLabel (billingDetails : BillingDetails) =
+    match billingDetails.Delivery with
+        | AsBilling -> billingDetails.Billing |> Some
+        | Physical address -> address |> Some
+        | Download -> None
+        | ClickAndCollect _ -> None
+        |> Option.map (fun address -> sprintf "%s\n%s" billingDetails.Name address)
+
+let deliveryLabels (billingDetails : BillingDetails seq) =
+            billingDetails 
+                |> Seq.choose tryDeliveryLabel
+
+let tryLastLine (address : string) =
+    let parts = address.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries)
+    match parts with
+        | [||] -> None
+        | parts -> parts |> Array.last |> Some
+
+let tryPostalCode (codeString : string) =
+    match Int32.TryParse(codeString) with
+        | true, i -> i |> Some
+        | false, _ -> None
+
+let postalCodeHub (code : int) = if code = 62291 then "Hub 1" else "Hub 2"
+
+let tryHub (billingDetails : BillingDetails) =
+    billingDetails
+        |> tryDeliveryLabel
+        |> Option.bind tryLastLine
+        |> Option.bind tryPostalCode
+        |> Option.map postalCodeHub
+
+let countNonNullBillingAddresses (orders : seq<BillingDetails>) =
+    orders
+        |> Seq.map (fun bd -> bd.Billing)
+        |> Seq.map Option.ofObj
+        |> Seq.sumBy Option.count
+
